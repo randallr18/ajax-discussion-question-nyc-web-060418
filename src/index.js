faae217be2fc87b9ab3215959676d6c7bf413d27
Risher Randall <randallr18@mail.wlu.edu>
2018-07-24 23:09:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+// hello
